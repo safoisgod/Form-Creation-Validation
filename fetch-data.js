f88e80964f90c0803e04b9f04e5c9136717aee3d
Nana Safo <iamnanasafo@gmail.com>
2025-08-03 16:33:22 +0000

    Initial commit: Form validation and API fetch project

---
+++ fetch-data.js
@@ -0,0 +1,26 @@
+const fetchUserData = async()=>{
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
+    const dataContainer = document.getElementById("api-data")
+
+    try{
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+        dataContainer.innerHTML = "";
+        const userList = document.createElement("ul");
+
+        users.forEach(user => {
+            const li = document.createElement("li");
+            li.textContent = user.name;
+            userList.appendChild(li);
+            
+        });
+        dataContainer.appendChild(userList)
+    }
+
+    catch (error){
+        dataContainer.innerHTML = ""
+        dataContainer.innerHTML= "Failed to load user data."
+    }
+}
+
+document.addEventListener("DOMContentLoaded",fetchUserData)
