df2dbbcbf1eaf1d496b01e848e84d221ecae0c99
Nana Safo <iamnanasafo@gmail.com>
2025-08-03 16:39:05 +0000

    corrected apiUrl

--- fetch-data.js
+++ fetch-data.js
@@ -1,26 +1,26 @@
-const fetchUserData = async()=>{
-    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
-    const dataContainer = document.getElementById("api-data")
+const fetchUserData = async () => {
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
+    const dataContainer = document.getElementById("api-data");
 
-    try{
+    try {
         const response = await fetch(apiUrl);
         const users = await response.json();
+
         dataContainer.innerHTML = "";
+
         const userList = document.createElement("ul");
 
         users.forEach(user => {
             const li = document.createElement("li");
             li.textContent = user.name;
             userList.appendChild(li);
-            
         });
-        dataContainer.appendChild(userList)
-    }
 
-    catch (error){
-        dataContainer.innerHTML = ""
-        dataContainer.innerHTML= "Failed to load user data."
+        dataContainer.appendChild(userList);
+    } catch (error) {
+        dataContainer.innerHTML = "";
+        dataContainer.textContent = "Failed to load user data.";
     }
-}
+};
 
-document.addEventListener("DOMContentLoaded",fetchUserData)
+document.addEventListener("DOMContentLoaded", fetchUserData);
